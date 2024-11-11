d5c37d0c765726a673667ee810b8625fc276d440
bruno.silva <john.doe@contoso.com>
2024-11-11 10:34:24 -0300

    add managed ssl certificate

--- main.tf
+++ main.tf
@@ -11,25 +11,49 @@
 #   region = "us-east1"
 # }
 
-
-# module "static-assets_cloud-storage-static-website" {
-#   source  = "gruntwork-io/static-assets/google//modules/cloud-storage-static-website"
-#   version = "0.6.0"
-  
-#   project = "rga-gcp-tech-assessment"
-#   website_domain_name = "test.base43.com.br"
-#   force_destroy_access_logs_bucket = true
-#   force_destroy_website = true
+# resource "google_compute_ssl_certificate" "static_web_cert" {
+#   project     = "rga-gcp-tech-assessment"
+#   name        = "static-web-cert"
+#   private_key = file("/home/bruno/rga-tech-assessment/jenkins/private.key")
+#   certificate = file("/home/bruno/rga-tech-assessment/jenkins/base43-cert.pem")
+#   lifecycle {
+#     create_before_destroy = true
+#   }
 # }
 
-resource "google_compute_ssl_certificate" "static_web_cert" {
-  project     = "rga-gcp-tech-assessment"
-  name        = "static-web-cert"
-  private_key = file("/home/bruno/rga-tech-assessment/jenkins/private.key")
-  certificate = file("/home/bruno/rga-tech-assessment/jenkins/base43-cert.pem")
-  lifecycle {
-    create_before_destroy = true
+locals {
+  name = "base43"
+  domain = "test.base43.com.br"
+}
+
+resource "google_certificate_manager_certificate" "default" {
+  name        = "${local.name}-rootcert"
+  description = "Cert with LB authorization"
+  managed {
+    domains = [local.domain]
   }
+  labels = {
+    "terraform" : true
+  }
+}
+
+resource "google_certificate_manager_certificate_map" "default" {
+  name        = "${local.name}-certmap1"
+  description = "${local.domain} certificate map"
+  labels = {
+    "terraform" : true
+  }
+}
+
+resource "google_certificate_manager_certificate_map_entry" "default" {
+  name        = "${local.name}-first-entry"
+  description = "example certificate map entry"
+  map         = google_certificate_manager_certificate_map.default.name
+  labels = {
+    "terraform" : true
+  }
+  certificates = [google_certificate_manager_certificate.default.id]
+  hostname     = local.domain
 }
 
 module "static-assets_http-load-balancer-website" {
@@ -42,5 +66,5 @@ module "static-assets_http-load-balancer-website" {
   force_destroy_website = true
 
   enable_ssl = true
-  ssl_certificate = google_compute_ssl_certificate.static_web_cert.self_link
+  ssl_certificate = "//certificatemanager.googleapis.com/${google_certificate_manager_certificate_map.default.id}"
 }
