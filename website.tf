c55f64f886ce3b6ba0a8404774b23a1bd16afbf5
bruno.silva <john.doe@contoso.com>
2024-11-12 10:56:18 -0300

    .

--- website.tf
+++ website.tf
@@ -1,13 +1,13 @@
-resource "google_storage_bucket_object" "indexpage" {
-  name         = "index.html"
-  content      = "<html><body>Hello World!</body></html>"
-  content_type = "text/html"
-  bucket       = module.static-assets_http-load-balancer-website.website_bucket_name
-}
+# resource "google_storage_bucket_object" "indexpage" {
+#   name         = "index.html"
+#   content      = "<html><body>Hello World!</body></html>"
+#   content_type = "text/html"
+#   bucket       = module.static-assets_http-load-balancer-website.website_bucket_name
+# }
 
-resource "google_storage_bucket_object" "errorpage" {
-  name         = "404.html"
-  content      = "<html><body>404!</body></html>"
-  content_type = "text/html"
-  bucket       = module.static-assets_http-load-balancer-website.website_bucket_name
-}
+# resource "google_storage_bucket_object" "errorpage" {
+#   name         = "404.html"
+#   content      = "<html><body>404!</body></html>"
+#   content_type = "text/html"
+#   bucket       = module.static-assets_http-load-balancer-website.website_bucket_name
+# }
